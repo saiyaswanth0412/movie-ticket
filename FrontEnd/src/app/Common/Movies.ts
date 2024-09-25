71bc9b9f40776562e34ca985a09fd49383b709c5
tharunkumarreddytummala <tharunkumarreddytummala>
2024-09-26 01:02:47 +0530

    Adjusted image postion

--- FrontEnd/src/app/Common/Movies.ts
+++ FrontEnd/src/app/Common/Movies.ts
@@ -56,7 +56,7 @@ export const moviesList = [
     {
       title: 'Movie 1',
       subtitle: 'Action',
-      image: 'https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-250,h-390/et00310216-aqlulhyqlt-portrait.jpg',
+      image: 'https://img.etimg.com/thumb/width-1200,height-900,imgsize-1214428,resizemode-75,msid-113144887/magazines/panache/ntr-jr-s-devara-part-1-trailer-date-revealed-as-movie-makes-pre-release-box-office-history-in-usa.jpg',
       details: 'A great action movieA great action movieA great action movie.'
     },
     {
