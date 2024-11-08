32fa52e617cf2a7cda411e9790f5921d16638ca6
Tharun <tharunkumarreddytummala@gmail.com>
2024-11-09 01:38:01 +0530

    added spinner in theatre page

--- FrontEnd/src/app/Components/theatre/theatre.component.ts
+++ FrontEnd/src/app/Components/theatre/theatre.component.ts
@@ -6,31 +6,35 @@ import { SpinnerService } from 'src/app/Services/spinner.service';
 @Component({
   selector: 'app-theatre',
   templateUrl: './theatre.component.html',
-  styleUrls: ['./theatre.component.css']
+  styleUrls: ['./theatre.component.css'],
 })
 export class TheatreComponent implements OnInit {
-
   selectedMovie: any = {};
 
-  constructor(private movieApiService: MovieApiService, private route: ActivatedRoute, public  spinnerService: SpinnerService) { }
+  constructor(
+    private movieApiService: MovieApiService,
+    private route: ActivatedRoute,
+    public spinnerService: SpinnerService
+  ) {}
 
   ngOnInit(): void {
     this.fetchMovies();
-   
   }
 
   private fetchMovies(): void {
-    this.spinnerService.show(); 
+    this.spinnerService.show();
     const movieId = this.route.snapshot.paramMap.get('id');
     if (movieId) {
       this.movieApiService.getMovie(movieId).subscribe({
         next: (movie) => this.selectedMovie = movie.selectedMovie,
         error: (error) => console.error('Error fetching movie:', error),
-        complete: () => console.log('Movie fetch complete')
-        
+        complete: () => {
+          console.log('Movie fetch complete');
+          this.spinnerService.hide();
+        },
       });
+    } else {
+      this.spinnerService.hide();
     }
-    this.spinnerService.hide();
   }
-  
 }
